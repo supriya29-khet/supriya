9f4c62f6220bd787802483790945eb2be48bce08
supriya29-khet <supriyakhetmalis1@gmail.com>
2026-05-06 22:45:58 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,15 @@
+num = 7
+is_prime = True
+
+if num <= 1:
+    is_prime = False
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
